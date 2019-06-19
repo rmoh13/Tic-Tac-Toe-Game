36712804c9af2c5aea0c76b365ec81e75006f905
Rishu Mohanka <rishu.mohanka@gmail.com>
2019-06-18 21:35:01 -0400

    improved user experience
    I added some colors and included names

--- main.py
+++ main.py
@@ -28,18 +28,18 @@ def render(board):
 #board[1][1] = 'O'
 #render(board)
 
-def get_move(num):
+def get_move(num, player1Name, player2Name):
     if num % 2 == 1:
-        x_coord = input("Player 1 (X): What is your move's X coordinate? ")
-        y_coord = input("Player 1 (X): What is your move's Y coordinate? ")
+        x_coord = input(player1Name + ": What is your move's X coordinate? ")
+        y_coord = input(player1Name + ": What is your move's Y coordinate? ")
         if "exit" in x_coord or "exit" in y_coord:
-            print("Aww, you forfeited! Player 2 (O) wins!")
+            print(Fore.RED + "Aww, you forfeited! %s wins!" % player2Name)
             sys.exit()
     else:
-        x_coord = input("Player 2 (O): What is your move's X coordinate? ")
-        y_coord = input("Player 2 (O): What is your move's Y coordinate? ")
+        x_coord = input(player2Name + ": What is your move's X coordinate? ")
+        y_coord = input(player2Name + ": What is your move's Y coordinate? ")
         if "exit" in x_coord or "exit" in y_coord:
-            print("Aww, you forfeited! Player 1 (X) wins!")
+            print(Fore.RED + "Aww, you forfeited! %s wins!" % player1Name)
             sys.exit()
     try:
         coords = (int(x_coord), int(y_coord))
@@ -153,30 +153,30 @@ def run_game():
     print("Welcome to Tic-Tac-Toe. The rules are the same, and you can type ''exit'' if you want to forfeit.")
     print("You can play against another person or an AI")
     option = input("Do you want to play against another player or an AI? ")
+    player1Name = input("Player 1 (X), Enter in your name: ")
+    player2Name = input("Player 2 (X), Enter in your name: ")
     if 'AI' not in option:
         i = 1
         while True:
             winner = get_winner(board)
-            if winner == 'X' or winner == 'O':
-                print(winner + " wins the game!")
+            if winner == 'X':
+                print(Fore.GREEN + player1Name + " wins the game!")
+                break
+            elif winner == 'O':
+                print(Fore.GREEN + player2Name + " wins the game!")
                 break
             if is_board_full(board, winner):
                 print("It's a draw!")
                 break
-            current_player = "X"
-            if i % 2 == 1:
-                current_player = "X"
-            else:
-                current_player = "O"
-            coords = get_move(i)
+            coords = get_move(i, player1Name, player2Name)
             while coords == False:
-                coords = get_move(i)
+                coords = get_move(i, player1Name, player2Name)
             b1 = board
             board = make_move(board, coords, i)
             while board == False:
-                coords = get_move(i)
+                coords = get_move(i, player1Name, player2Name)
                 while coords == False:
-                    coords = get_move(i)
+                    coords = get_move(i, player1Name, player2Name)
                 board = make_move(b1, coords, i)
             render(board)
             i += 1
