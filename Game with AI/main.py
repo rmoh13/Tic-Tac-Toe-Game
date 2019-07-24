38f75f16552531d52f60f94e03d8794effd7cc7c
Rishu Mohanka <rishu.mohanka@gmail.com>
2019-07-24 00:24:11 -0400

    This is a better version with more AIs available

--- Game with AI/main.py
+++ Game with AI/main.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from colorama import Fore, Back, Style
 
 def new_board():
@@ -148,42 +149,342 @@ def is_board_full(board, possible_winner):
         sum = 0
         for i in range(0, len(board)):
             for j in range(0, len(board[0])):
-                if board[i][j] == 'X' or board[i][j] == 'O':
+                if board[i][j] == u"\u001b[36mX" or board[i][j] == u"\u001b[33mO":
                     sum += 1
         if sum == len(board) * len(board[0]):
             return True
 
+def random_AI(board, current_player):
+    coord_options = []
+    for i in range(0, len(board)):
+        for j in range(0, len(board[0])):
+            if board[i][j] == " ":
+                coord_options.append((i,j))
+    coords = random.choice(coord_options)
+    return coords
+'''
+board = [
+  ['X', 'O', None],
+  ['O', 'O', None],
+  ['X', None, None]
+]
+print(random_AI(board))
+'''
+
+def finds_winning_moves_AI(board, turn):
+    if turn % 2 == 1:
+        current_player = u"\u001b[36mX"
+    else:
+        current_player = u"\u001b[33mO"
+    winning_spot = False
+    list_all_lines = []
+    list_all_lines_coords = []
+    list_all_lines_coords.append([(0,0), (1,0), (2,0)])
+    list_all_lines_coords.append([(0,1), (1,1), (2,1)])
+    list_all_lines_coords.append([(0,2), (1,2), (2,2)])
+    [list_all_lines.append(i) for i in board]
+
+    diag1 = []
+    diag1.append(board[0][0])
+    diag1.append(board[1][1])
+    diag1.append(board[2][2])
+    list_all_lines.append(diag1)
+    list_all_lines_coords.append([(0,0), (1,1), (2,2)])
+    diag2 = []
+    diag2.append(board[0][2])
+    diag2.append(board[1][1])
+    diag2.append(board[2][0])
+    list_all_lines.append(diag2)
+    list_all_lines_coords.append([(2,0), (1,1), (0,2)])
+
+    for i in range(0, len(board[0])):
+        col = []
+        col_coord = []
+        for j in range(0, len(board)):
+            col.append(board[j][i])
+            col_coord.append((i,j))
+        list_all_lines.append(col)
+        list_all_lines_coords.append(col_coord)
+    #print(list_all_lines)
+    for i in range(0, len(list_all_lines_coords)):
+        # sets must have distinct elements
+        similarity_counter = 0
+        for j in range(0, len(list_all_lines_coords[0])):
+            if board[list_all_lines_coords[i][j][0]][list_all_lines_coords[i][j][1]] == current_player:
+                similarity_counter += 1
+        if similarity_counter == 2:
+            for j in range(0, len(list_all_lines_coords[0])):
+                if board[list_all_lines_coords[i][j][0]][list_all_lines_coords[i][j][1]] == " ":
+                    winning_spot = list_all_lines_coords[i][j]
+    if winning_spot == False:
+        coords = random_AI(board, turn)
+        return coords
+    else:
+        coords = winning_spot
+        return coords
+'''
+board = [
+  [u"\u001b[36mX", u"\u001b[33mO", " "],
+  [" ", u"\u001b[33mO", " "],
+  [u"\u001b[36mX", " ", " "]
+]
+print(finds_winning_moves_AI(board, 2))
+'''
+
+def finds_winning_and_losing_moves_AI(board, turn):
+    if turn % 2 == 1:
+        current_player = u"\u001b[36mX"
+        opponent = u"\u001b[33mO"
+    else:
+        current_player = u"\u001b[33mO"
+        opponent = u"\u001b[36mX"
+    coord1 = finds_winning_moves_AI(board, turn)
+    coord2 = finds_winning_moves_AI(board, turn + 1)
+    coord3 = random_AI(board, turn)
+
+def human_player(board, turn):
+    x_coord = input("What is your move's X coordinate? ")
+    y_coord = input("What is your move's Y coordinate? ")
+    if "exit" in x_coord or "exit" in y_coord:
+        print(Fore.RED + "Aww, you forfeited!")
+        sys.exit()
+    try:
+        coords = (int(x_coord), int(y_coord))
+    except ValueError:
+        print("You didn't type in a number! Try again.")
+        return False
+    return coords
+
+'''
+The below code before run_game() is just to look at average win percentages for the different AIs pitted up against each other.
+'''
+def play(first_AI, second_AI):
+    if "random" in first_AI and "slight" in second_AI:
+        i = 1
+        board = new_board()
+        render(board)
+        while True:
+            if i % 2 == 1:
+                winner = get_winner(board)
+                if winner == u"\u001b[36mX":
+                    print(Fore.GREEN + u"\u001b[36mRandom" + " wins the game!")
+                    break
+                elif winner == u"\u001b[33mO":
+                    print(Fore.GREEN + u"\u001b[33mSlightly Intelligent AI" + " wins the game!")
+                    break
+                if is_board_full(board, winner):
+                    print("It's a draw!")
+                    break
+                coords = random_AI(board, i)
+                while coords == False:
+                    coords = random_AI(board, i)
+                b1 = board
+                board = make_move(board, coords, i)
+                while board == False:
+                    coords = random_AI(board, i)
+                    while coords == False:
+                        coords = random_AI(board, i)
+                    board = make_move(b1, coords, i)
+                render(board)
+            else:
+                winner = get_winner(board)
+                if winner == u"\u001b[36mX":
+                    print(Fore.GREEN + u"\u001b[36mX" + " wins the game!")
+                    break
+                elif winner == u"\u001b[33mO":
+                    print(Fore.GREEN + u"\u001b[33mO" + " wins the game!")
+                    break
+                if is_board_full(board, winner):
+                    print("It's a draw!")
+                    break
+                coords = finds_winning_moves_AI(board, i)
+                while coords == False:
+                    coords = finds_winning_moves_AI(board, i)
+                b1 = board
+                board = make_move(board, coords, i)
+                while board == False:
+                    coords = finds_winning_moves_AI(board, i)
+                    while coords == False:
+                        coords = finds_winning_moves_AI(board, i)
+                    board = make_move(b1, coords, i)
+                render(board)
+            i += 1
+
+        if winner == u"\u001b[36mX":
+            return 1
+        elif winner == u"\u001b[33mO":
+            return 2
+        else:
+            return 0
+
+def repeated_battle(N, first_AI, second_AI):
+    sum = 0
+    for i in range(0, N):
+        sum += play(first_AI, second_AI)
+    average = sum / N
+    return average
+
+#print(repeated_battle(50, "random", "slightly"))
+
 def run_game():
     board = new_board()
+    print(board)
     render(board)
     print("Welcome to Tic-Tac-Toe. The rules are the same, and you can type ''exit'' if you want to forfeit.")
     print("You can play against another person or an AI")
-    option = input("Do you want to play against another player or an AI? ")
-    player1Name = input("Player 1 (X), Enter in your name: ")
-    player2Name = input("Player 2 (O), Enter in your name: ")
-    if 'AI' not in option:
-        i = 1
-        while True:
-            winner = get_winner(board)
-            if winner == 'X':
-                print(Fore.GREEN + player1Name + " wins the game!")
-                break
-            elif winner == 'O':
-                print(Fore.GREEN + player2Name + " wins the game!")
-                break
-            if is_board_full(board, winner):
-                print("It's a draw!")
-                break
-            coords = get_move(i, player1Name, player2Name)
-            while coords == False:
-                coords = get_move(i, player1Name, player2Name)
-            b1 = board
-            board = make_move(board, coords, i)
-            while board == False:
+    option = input("Do you want to watch different AIs battle against each other OR play against another player, a slightly intelligent AI, a very intelligent AI, or a dumb AI making random moves? ")
+    if 'watch' not in option:
+        if 'player' in option:
+            player1Name = input("Player 1 (X), Enter in your name: ")
+            player2Name = input("Player 2 (O), Enter in your name: ")
+            i = 1
+            while True:
+                winner = get_winner(board)
+                if winner == u"\u001b[36mX":
+                    print(Fore.GREEN + player1Name + " wins the game!")
+                    break
+                elif winner == u"\u001b[33mO":
+                    print(Fore.GREEN + player2Name + " wins the game!")
+                    break
+                if is_board_full(board, winner):
+                    print("It's a draw!")
+                    break
                 coords = get_move(i, player1Name, player2Name)
                 while coords == False:
                     coords = get_move(i, player1Name, player2Name)
-                board = make_move(b1, coords, i)
-            render(board)
-            i += 1
+                b1 = board
+                board = make_move(board, coords, i)
+                while board == False:
+                    coords = get_move(i, player1Name, player2Name)
+                    while coords == False:
+                        coords = get_move(i, player1Name, player2Name)
+                    board = make_move(b1, coords, i)
+                render(board)
+                i += 1
+        elif 'random' in option:
+            i = 1
+            while True:
+                winner = get_winner(board)
+                if winner == u"\u001b[36mX":
+                    print(Fore.GREEN + u"\u001b[36mX" + " wins the game!")
+                    break
+                elif winner == u"\u001b[33mO":
+                    print(Fore.GREEN + u"\u001b[33mO" + " wins the game!")
+                    break
+                if is_board_full(board, winner):
+                    print("It's a draw!")
+                    break
+                coords = random_AI(board, i)
+                while coords == False:
+                    coords = random_AI(board, i)
+                b1 = board
+                board = make_move(board, coords, i)
+                while board == False:
+                    coords = random_AI(board, i)
+                    while coords == False:
+                        coords = random_AI(board, i)
+                    board = make_move(b1, coords, i)
+                render(board)
+                i += 1
+        elif 'slight' in option:
+            i = 1
+            while True:
+                winner = get_winner(board)
+                if winner == u"\u001b[36mX":
+                    print(Fore.GREEN + u"\u001b[36mX" + " wins the game!")
+                    break
+                elif winner == u"\u001b[33mO":
+                    print(Fore.GREEN + u"\u001b[33mO" + " wins the game!")
+                    break
+                if is_board_full(board, winner):
+                    print("It's a draw!")
+                    break
+                coords = finds_winning_moves_AI(board, i)
+                while coords == False:
+                    coords = finds_winning_moves_AI(board, i)
+                b1 = board
+                board = make_move(board, coords, i)
+                while board == False:
+                    coords = finds_winning_moves_AI(board, i)
+                    while coords == False:
+                        coords = finds_winning_moves_AI(board, i)
+                    board = make_move(b1, coords, i)
+                render(board)
+                i += 1
+    else:
+        option2 = input("\nWhich AIs from the aforementioned list do you want to watch? The second player is the slightly intelligent by default.\nEnter here: ")
+        if 'random' in option2:
+            i = 1
+            while True:
+                if i % 2 == 1:
+                    winner = get_winner(board)
+                    if winner == u"\u001b[36mX":
+                        print(Fore.GREEN + u"\u001b[36mX" + " wins the game!")
+                        break
+                    elif winner == u"\u001b[33mO":
+                        print(Fore.GREEN + u"\u001b[33mO" + " wins the game!")
+                        break
+                    if is_board_full(board, winner):
+                        print("It's a draw!")
+                        break
+                    coords = random_AI(board, i)
+                    while coords == False:
+                        coords = random_AI(board, i)
+                    b1 = board
+                    board = make_move(board, coords, i)
+                    while board == False:
+                        coords = random_AI(board, i)
+                        while coords == False:
+                            coords = random_AI(board, i)
+                        board = make_move(b1, coords, i)
+                    render(board)
+                else:
+                    winner = get_winner(board)
+                    if winner == u"\u001b[36mX":
+                        print(Fore.GREEN + u"\u001b[36mX" + " wins the game!")
+                        break
+                    elif winner == u"\u001b[33mO":
+                        print(Fore.GREEN + u"\u001b[33mO" + " wins the game!")
+                        break
+                    if is_board_full(board, winner):
+                        print("It's a draw!")
+                        break
+                    coords = finds_winning_moves_AI(board, i)
+                    while coords == False:
+                        coords = finds_winning_moves_AI(board, i)
+                    b1 = board
+                    board = make_move(board, coords, i)
+                    while board == False:
+                        coords = finds_winning_moves_AI(board, i)
+                        while coords == False:
+                            coords = finds_winning_moves_AI(board, i)
+                        board = make_move(b1, coords, i)
+                    render(board)
+                i += 1
+        elif 'slight' in option2:
+            i = 1
+            while True:
+                winner = get_winner(board)
+                if winner == u"\u001b[36mX":
+                    print(Fore.GREEN + u"\u001b[36mX" + " wins the game!")
+                    break
+                elif winner == u"\u001b[33mO":
+                    print(Fore.GREEN + u"\u001b[33mO" + " wins the game!")
+                    break
+                if is_board_full(board, winner):
+                    print("It's a draw!")
+                    break
+                coords = finds_winning_moves_AI(board, i)
+                while coords == False:
+                    coords = finds_winning_moves_AI(board, i)
+                b1 = board
+                board = make_move(board, coords, i)
+                while board == False:
+                    coords = finds_winning_moves_AI(board, i)
+                    while coords == False:
+                        coords = finds_winning_moves_AI(board, i)
+                    board = make_move(b1, coords, i)
+                render(board)
+                i += 1
+
 run_game()
